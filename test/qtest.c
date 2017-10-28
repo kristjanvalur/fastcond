19826852fbf36a2803d60357ee42ab39a14579e3
Kristján Valur Jónsson <sweskman@gmail.com>
2017-10-28 13:28:26 +0000

    Added tracking of latency to qtest.c

--- test/qtest.c
+++ test/qtest.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <time.h>
+#include <math.h>
 
 #include "fastcond.h"
 #ifdef TEST_WCOND
@@ -22,10 +24,15 @@
 typedef struct _queue
 {
 	pthread_mutex_t mutex;
-	int n_data; /* current size of queue */
+	
+	struct timespec *queue; /* queue contains times when it was put in */
+	int s_queue;   /* max size of queue */
+	int i_queue;
+	int n_queue; /* current size of queue */
+
+
 	pthread_cond_t not_empty;
 	pthread_cond_t not_full;
-	int max_queue;  /* how much fits in the queue */
 	int max_send;  /* how many packets to send? (termination test) */
     int n_sent; /* total amount sent */
 } queue_t;
@@ -53,11 +60,15 @@ void *sender(void *arg)
 			pthread_mutex_lock(&q->mutex);
 			have_data = 1;
 		}
-		while(q->n_sent < q->max_send && q->n_data >= q->max_queue)
+		while(q->n_sent < q->max_send && q->n_queue >= q->s_queue)
 			/* queueu is active and full */
 			pthread_cond_wait(&q->not_full, &q->mutex);
-		if (q->n_sent < q->max_send){
-			q->n_data++;
+		if (q->n_sent < q->max_send && q->n_queue < q->s_queue){
+			/* put stuff on queue */
+			int i = (q->i_queue + q->n_queue) % q->s_queue;
+			clock_gettime(CLOCK_MONOTONIC, &q->queue[i]);
+			q->n_queue++;
+
 			q->n_sent++;
 			n_sent++;
 			have_data = 0;
@@ -80,27 +91,55 @@ void *receiver(void *arg)
 	queue_t *q = args->queue;
 	int n_got =0;
 	int have_data = 0;
+	float time, sum_time=0.0, sum_time2=0.0; /* stats */
+	struct timespec data, now; 
 	pthread_mutex_lock(&q->mutex);
-	while (q->n_sent < q->max_send || q->n_data) {
+	while (q->n_sent < q->max_send || q->n_queue) {
 		if (have_data) {
 			/* simulate getting rid of the data */
 			pthread_mutex_unlock(&q->mutex);
+			/* compute the delay */
+			now.tv_sec -= data.tv_sec;
+			now.tv_nsec -= data.tv_nsec;
+			if (now.tv_nsec < 0) { /* rollover */
+				now.tv_sec --;
+				now.tv_nsec += 1000000000;
+			}
+			/* comute stats */
+			time = (float)now.tv_sec + (float)now.tv_nsec * 1e-9f;
+			sum_time += time;
+			sum_time2 += time * time;
 			sched_yield();
 			pthread_mutex_lock(&q->mutex);
 			have_data = 0;
 		}
-		while(q->n_sent < q->max_send && !q->n_data)
-			/* queueu is active and full */
+		while(q->n_sent < q->max_send && !q->n_queue)
+			/* queueu is active and empty */
 			pthread_cond_wait(&q->not_empty, &q->mutex);
-		if (q->n_data){
-			q->n_data--;
+		if (q->n_queue){
+			/* remove from queue */
+			clock_gettime(CLOCK_MONOTONIC, &now);
+			data = q->queue[q->i_queue];
+			q->i_queue = (q->i_queue + 1) % q->s_queue;
+			q->n_queue--;
+
 			n_got++;
 			have_data = 1;
 			pthread_cond_signal(&q->not_full);
+
 		}
 	}
 	pthread_mutex_unlock(&q->mutex);
-	printf("receiver %d got %d\n", args->id, n_got);
+	/* compute stats */
+	{
+		float avg=0.0f, sigma=0.0f;
+		if (n_got) {
+			avg = sum_time / (float)n_got;
+			sigma = sqrtf((float)n_got * sum_time - sum_time2) / (float)n_got;
+		}
+		
+		printf("receiver %d got %d latency avg %e stdev %e\n", args->id, n_got, avg, sigma);
+	}
 	return NULL;
 }
 
@@ -109,7 +148,7 @@ int main(int argc, char*argv[])
 	int n_data = 1000;
 	int n_senders = 1;
 	int n_receivers = 1;
-	int max_queue = 10;
+	int s_queue = 10;
 	queue_t q;
 	int i;
 	args_t *senders, *receivers;
@@ -120,12 +159,16 @@ int main(int argc, char*argv[])
 	if (argc > 2)
 		n_senders = atoi(argv[2]);
 	if (argc > 3)
-		max_queue = atoi(argv[3]);
+		s_queue = atoi(argv[3]);
 	n_receivers = n_senders;
 
 
-	q.n_data = q.n_sent = 0;
-	q.max_queue = max_queue;
+	/* init the queue */
+	q.i_queue = q.n_queue = 0;
+	q.s_queue = s_queue;
+	q.queue = (struct timespec *)malloc(s_queue * sizeof(struct timespec));
+	
+	q.n_sent = 0;
 	q.max_send = n_data;
 	pthread_mutex_init(&q.mutex, NULL);
 	pthread_cond_init(&q.not_empty, NULL);
